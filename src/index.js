939c98841d60b80c6f2b20b95b96b7831a02dff4
David Marquez <djmbdv@gmail.com>
2026-01-20 23:27:50 -0400

    chore: vercel node 24 + api handler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createApp } from './app.js';
 const app = createApp();
-app.listen(port, () => {
+// if is running in vercer ignore listen
+if (!process.env.VERCEL) {
+  app.listen(port, () => {
   console.log(`VICIphone Node server listening on port ${port}`);
 });
+  
+}
+
 
 export default app;
